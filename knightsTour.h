51d752f2c88d20fc2113a31cc6f7087a5fa90e2d
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2024-03-18 00:25:56 +0200

    Added new functions

--- knightsTour.h
+++ knightsTour.h
@@ -1,13 +1,25 @@
 #ifndef KNIGHTSTOUR_H
 #define KNIGHTSTOUR_H
 
-typedef struct {
+typedef struct Location Location;
+
+struct Location {
     int x, y;
-    int isValid;
-    struct Location* validOptions;
-} Location;
+    int isValid; //1 - I can go, 0 - IDK, -1 - Can not go
+    int validMoves; //Number of valid moves
+    int score; //Number of valid moves
+    Location *validOptions; //Squares that we can go to
+};
 
 void initBoard(Location ***board, int N);
+
+void findValidSquares(Location **board, Location square, int N);
+
+void move(Location **board, Location *square);
+
+void finalMove(Location **board, Location *square);
+
+int finished(Location **board, int N);
 
 void freeBoard(Location ***board, int N);
 
