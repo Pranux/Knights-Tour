9d4af05d038fc2949a36c83ca36f2acfe88433cb
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2024-03-18 01:25:21 +0200

    Now user can type in his own size of a chess board

--- main.c
+++ main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include "knightsTour.h"
-#define N 8
+
+void gettingN(int *N) {
+    printf("Enter a size of a chess board: ");
+    if(scanf("%d", N) != 1 || *N <= 4 || getchar() != '\n') {
+        printf("N must be an integer, higher than a 4.");
+        return;
+    }
+}
 
 int main(void) {
     Location **board;
+    int N = 0;
+    int iteration = 0;
+
+    gettingN(&N);
+    if(N == 0) { return 0; }
     initBoard(&board, N);
     Location current = board[0][0];
-    int iteration = 0;
 
     printf("Moving pattern:\n");
 
